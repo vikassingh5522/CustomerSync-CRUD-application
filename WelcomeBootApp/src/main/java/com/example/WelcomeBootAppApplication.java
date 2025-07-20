3e59731cc9eb2ad932627e251f3aaac3b6d08fbb
vikas singh <135737417+vikassingh5522@users.noreply.github.com>
2025-07-20 15:02:24 +0530

    Add files via upload

---
+++ WelcomeBootApp/src/main/java/com/example/WelcomeBootAppApplication.java
@@ -0,0 +1,11 @@
+package com.example;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelcomeBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelcomeBootAppApplication.class, args);
+	}
+}
